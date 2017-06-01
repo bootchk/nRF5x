14c77a6fa3f36a582e653eb3ec59dd7970a1e474
bootchk <bootch@nc.rr.com>
2017-06-01 12:04:35 -0400

    Bug in 64-bit clock: use Lamport's Rule

--- src/modules/longClockTimer.cpp
+++ src/modules/longClockTimer.cpp
@@ -21,12 +21,17 @@ Counter counter;
 
 
 
-// MSB of LongClock incremented by overflow interrupt handler
-uint32_t mostSignificantBits;
+/*
+ * MSB of LongClock
+ * !!! Volatile: incremented by overflow interrupt handler (a thread), and read by main thread.
+ * See below re disable interrupts for critical sections.
+ */
+volatile uint32_t mostSignificantBits;
 
 /*
- * RTC device has three compare registers.
- * This class owns them also, and a callback for each's interrupt.
+ * RTC device has three compare registers, each generating event/interrupt.
+ * All the events are handled by one ISR (RTC0_ISRHandler)
+ * This class owns a facade on the counter registers, and a knows a callback for each's interrupt.
  * Callback is also used as a flag for 'started'
  */
 TimerCallback timerCallback[3];
@@ -110,6 +115,7 @@ RTC0_IRQHandler(void)
 		mostSignificantBits++;
 		counter.clearOverflowEvent();
 		// assert interrupt still enabled
+		// assert counter is near zero (it rolled over just before the interrupt)
 	}
 
 	// Source event is compare register match event
@@ -195,12 +201,44 @@ void LongClockTimer::resetToNearZero(){
 
 
 LongTime LongClockTimer::nowTime() {
-	OSTime currentOSClockTicks = counter.ticks();
-	// Concatenate MSB and LSB.  Portable?
-	LongTime result = mostSignificantBits;
+
+	/*
+	 * Implementation: use Lamport's Rule.
+	 * To correctly catenate two 32-bit (sic) components of the 64-bit clock.
+	 *
+	 * The components are volatile and incremented by separate threads.
+	 * ISR increments mostSignificantBits (separate thread, at any instant.)
+	 * leastSignificantBits are incremented by hw (separate thread.)
+	 * This routine is in a third separate thread (main thread.)
+	 */
+	uint32_t firstMSBRead, secondMSBRead;
+	OSTime LSBRead;
+	do {
+		firstMSBRead = mostSignificantBits;
+		LSBRead = counter.ticks();
+		secondMSBRead = mostSignificantBits;
+	}
+	while (firstMSBRead != secondMSBRead);
+
+	/*
+	 * Catenate MSB and LSB reads.  Portable?
+	 */
+	LongTime result = firstMSBRead;
 	result = result << OSClockCountBits;	// Left shift result, fill LSB with zero
-	result = result | currentOSClockTicks;	// Bit-wise OR into LSB.  Addition would work also.
+	/*
+	 * Bit-wise OR the 32-bit LSBRead into lower 32-bit of result.  Addition would work also.
+	 * Assert !(LSBRead & 0xFF000000) i.e. high order 8-bits are zeroes
+	 */
+	result = result | LSBRead;
 	return result;
+
+	// XXX Monotonicity by design, but add an assertion here
+	/*
+	 * i.e.
+	 * priorResult initially 0
+	 * assert(thisResult > priorResult)
+	 * priorResult = thisResult
+	 */
 }
 
 
--- src/modules/longClockTimer.h
+++ src/modules/longClockTimer.h
@@ -6,10 +6,69 @@
 
 
 /*
- * Type LongClock yields.  Only 56 bits are valid.
+ * Thin wrapper around RTC device of Nordic nRF52/51.
+ * This defines an API that might be portable.
+ *
+ *
+ *
+ * Provides:
+ * - LongClock 56-bit counter. At resolution of 30uSec, overflows in 67,000 years.
+ * - Three Timers, 24-bit, one-shot. At resolution of 30uSec, max timeout is 5 seconds.
+ * !!! Timers are shorter than the LongClock.
+ *
+ * Units (precision) is ticks of 30uSec (from the crystal LF clock.)
+ *
+ * Accuracy is the same as for the crystal LF clock (say 20ppm.)
+ *
+ * Returns the current value of this machines low(?) resolution time source.
+ * It bears no relation to:
+ * - any other machines time source,
+ * - to any other time source on the machine,
+ * - or to wall time.
+ * In particular, one machine's time source can drift at rate of specified accuracy, in relation to other machine's time source.
+ *
+ * Resolution:
+ * - successive nowTime() calls may return the same value .
+ * - successive nowTime() calls may return values whose difference is greater than one tick (no guarantee that this code is fast.)
+ * - successive nowTime() calls spanning greater than (2**56-1)*30uSec  (about 68,000 years) will be incorrect due to 56-bit rollover.
+ *
+ * Monotonicity:
+ * - a nowTime() call result is >= result from a prior call !!!!
+ * (unless you call resetToNearZero() .)
+ *
+ * Timer durations:
+ * - min and max specified below
+ * Max timeout is (2**24-1)*30uSec ~=8.39minutes
+ *
+ * Legal call sequences:
+ *
+ * Normal:  init, getNowTime, startTimer, cancelTimer
+ * You can cancel a Timer that has not been started: init, cancelTimer
+ *
+ * Each timer has a callback that occurs while interrupts are disabled (called from within the ISR.)
+ * Here the three Timers are distinguished in the ISR:
+ * - First timer is used for low-power sleep functionality
+ * - other timers do all their processing within the callback (it should be short.)
+ * All the Timers wake up the mcu from sleep.
+ *
+ * Implementation:
+ *
+ * Owns private data defined in anon namespace.
+ *
+ * 24-bit RTC device counter underlies both LongClock and Timers.
+ *
+ * To insure monotonicity and correctness, uses Lamport's Rule to deal with for internal counter rollover.
+ */
+
+
+/*
+ * Type that LongClock yields.  Only 56 bits are valid.
  */
 typedef uint64_t LongTime;
 
+/*
+ * Type that holds the largest timeout for Timers.
+ */
 typedef uint32_t OSTime;
 
 
@@ -38,25 +97,7 @@ typedef void (*TimerCallback)(TimerInterruptReason);
 
 
 
-/*
- * Thin wrapper around RTC device of Nordic nRF52/51.
- * This defines an API that might be portable.
- *
- * Owns private data defined in anon namespace.
- *
- * Provides:
- * - LongClock 56-bit counter. At resolution of 30uSec, overflows in 67,000 years.
- * - Three Timers, 24-bit, one-shot. At resolution of 30uSec, max timeout is 5 seconds.
- *
- * !!! Timers are shorter than the LongClock.
- * 24-bit RTC device counter underlies both LongClock and Timers,
- * but LongClock extends the clock to 56-bits.
- *
- * Legal call sequences:
- *
- * Normal:  init, getNowTime, startTimer, cancelTimer
- * You can cancel a Timer that has not been started: init, cancelTimer
- */
+
 class LongClockTimer {
 	static const int OSClockCountBits = 24;
 
@@ -65,7 +106,8 @@ public:
 	static const unsigned int MaxTimeout = 0xFFFFFF;
 
 	// Device won't reliably cause event for timeouts < 2
-	// Some authors use 3 for safety
+	// Some authors use 3 ???
+	// TODO revisit Product Spec
 	// app_timer used 5, because it had other delays?
 	static const unsigned int MinTimeout = 2;
 
@@ -75,6 +117,12 @@ public:
 	 */
 	static const unsigned int CountTimerInstances = 3;
 
+
+
+
+	/*
+	 * Clock methods.
+	 */
 	/*
 	 * init does not guarantee clock is running (only that it is started.)
 	 * You can start a timer even if clock is not running, but it may delay longer than requested,
@@ -85,6 +133,7 @@ public:
 	static void init(Nvic*);
 
 	/*
+	 * AKA Reset the clock, but not to zero.
 	 * Make high order bits zero.
 	 * Low order bits are whatever the HW counter is.
 	 */
@@ -92,6 +141,10 @@ public:
 
 	static LongTime nowTime();
 
+
+	/*
+	 * Timer methods.
+	 */
 	static void startTimer(
 			TimerIndex index,	// [0:2]
 			OSTime timeout, // [0:0xffffff]
