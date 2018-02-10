6358d2e7b6caa7fc138436b5d2476e753d0df5b1
bootchk <bootch@nc.rr.com>
2018-02-09 19:20:07 -0500

    Config LFTimner for NRF51

--- src/drivers/hwConfig.h
+++ src/drivers/hwConfig.h
@@ -9,11 +9,12 @@
  * Nordic docs wrong: 51 has no RTC2
  */
 #ifdef NRF52
-#define LFTimerUseRTC2    1
+#define LFTimerUseRTC2    1	// This impacts upstream radioSoC
 #define LFTimerRTC        NRF_RTC2
 #define LFTimerRTCIRQ     RTC2_IRQn
 #else
 // Not SD compatible on 51?  Conflicts with app_timer?
+#define LFTimerUseRTC1    1
 #define LFTimerRTC        NRF_RTC1
 #define LFTimerRTCIRQ     RTC1_IRQn
 #endif
